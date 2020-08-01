ebb2905096ec74a8d690d33e87a4d7bb25f90f82
Onza <12499787+norecords@users.noreply.github.com>
2020-08-01 10:59:52 +0200

    Compatible with Python 3.8.2 and Weewx 4.1.1
    I made some changes to make it work again ;)

--- bin/user/wxMesh.py
+++ bin/user/wxMesh.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# weewx driver that reads data from MQTT subscription
+# weewx driver that reads data from MQTT subscription for Python 3 and Weewx 4.x
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -40,7 +40,7 @@
 from __future__ import with_statement
 import syslog
 import time
-import Queue
+import queue
 import paho.mqtt.client as mqtt
 import weewx.drivers
 
@@ -63,7 +63,7 @@ def _get_as_float(d, s):
     if s in d:
         try:
             v = float(d[s])
-        except ValueError, e:
+        except ValueError as e:
             logerr("cannot read value for '%s': %s" % (s, e))
     return v
 
@@ -92,35 +92,39 @@ class wxMesh(weewx.drivers.AbstractDevice):
       loginf("polling interval is %s" % self.poll_interval)
       loginf('label map is %s' % self.label_map)
       
-      self.payload = Queue.Queue()
+      self.payload = queue.Queue()
       self.connected = False
 
-      self.client = mqtt.Client(client_id=self.client_id, protocol=mqtt.MQTTv31)
+      self.client = mqtt.Client(client_id="weewxMesh", clean_session=False, userdata=None, protocol=mqtt.MQTTv311, transport="tcp")
+
+      self.client.on_connect = self.on_connect
+      self.client.on_message = self.on_message
+
 
       # TODO - need some reconnect on disconnect logic
       #self.client.on_disconnect = self.on_disconnect
-      self.client.on_message = self.on_message
 
       self.client.username_pw_set(self.username, self.password)
       self.client.connect(self.host, 1883, 60)
-      # TODO is this a good idea?
-      # while self.connected != True:
-      #    time.sleep(1)
-      #    logdbg("Connecting...\n")
-      
-      logdbg("Connected")
       self.client.loop_start()
-      self.client.subscribe(self.topic, qos=1)
+
+    # The callback for when the client rEceives a CONNACK response from the server.
+    def on_connect(self, client, userdata, flags, rc):
+        loginf("Connected on mqtt server with result code "+str(rc))
+        if rc == 0:
+            self.connected = True
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        self.client.subscribe(self.topic)
+
 
     # The callback for when a PUBLISH message is received from the MQTT server.
     def on_message(self, client, userdata, msg):
-      self.payload.put(msg.payload,)
+      msg.payload = msg.payload.decode('UTF-8')
+      self.payload.put(msg.payload, block=True, timeout=None)
+
       logdbg("Added to queue of %d message %s" % (self.payload.qsize(), msg.payload))
 
-    def on_connect(self, client, userdata, rc):
-      if rc == 0:
-	self.connected = True
-        
     def closePort(self):
       self.client.disconnect()
       self.client.loop_stop()
@@ -128,29 +132,29 @@ class wxMesh(weewx.drivers.AbstractDevice):
     def genLoopPackets(self):
       while True:
 	# read whatever values we can get from the MQTT broker
-	logdbg("Queue of %d entries" % self.payload.qsize())
-	logdbg("Waiting for non-empty queue")
+        logdbg("Queue of %d entries" % self.payload.qsize())
+        logdbg("Waiting for non-empty queue")
         while not self.payload.empty(): 
           msg = str(self.payload.get(block=True, timeout=3)) # block until something gets queued
-	  logdbg("Working on queue of size %d with payload : %s" % (self.payload.qsize(), msg))
-	  data = {}
-	  row = msg.split(",")
-	  for datum in row:
-	    (key,value)  = datum.split(":")
-	    data[key] = value
-	    if( key=="TIME" and data[key] == "0"):
-	      data[key] = str(int(time.time())) # time from station is not yet reliable - replace it
-	    logdbg("key: "+key+" value: "+data[key])
-            
+          logdbg("Working on queue of size %d with payload : %s" % (self.payload.qsize(), msg))
+          data = {}
+          row = msg.split(",")
+          for datum in row:
+            (key,value)  = datum.split(":")
+            data[key] = value
+            if( key=="TIME" and data[key] == "0"):
+              data[key] = str(int(time.time())) # time from station is not yet reliable - replace it
+            logdbg("key: "+key+" value: "+data[key])
+
 	  # map the data into a weewx loop packet
-	  _packet = {'usUnits': weewx.METRIC}
-	  for vname in data:
-	    _packet[self.label_map.get(vname, vname)] = _get_as_float(data, vname)
+          _packet = {'usUnits': weewx.METRIC}
+          for vname in data:
+            _packet[self.label_map.get(vname, vname)] = _get_as_float(data, vname)
 
-	  yield _packet
+          yield _packet
 
-	logdbg("Sleeping for %d" % self.poll_interval)
-	time.sleep(self.poll_interval)
+        logdbg("Sleeping for %d" % self.poll_interval)
+        time.sleep(self.poll_interval)
 
       self.client.disconnect()
       self.client.loop_stop()
